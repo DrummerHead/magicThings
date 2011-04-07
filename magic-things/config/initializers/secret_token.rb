a4979d219406743704dec4f41ec283e5d34ef42d
Nicolas Barrera <dev+DrummerHead@cuboxsa.com>
2011-04-07 20:00:48 -0300

    starting a test ruby coso

---
+++ magic-things/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MagicThings::Application.config.secret_token = 'f20120494b21057fbe4b4fa74dae6da4e46668346b0311b5789b841b9bf7d1233659394b18b1e2b23b64b358fd1d5c50bca53952489a03a08b79f2e080e6d7e9'
